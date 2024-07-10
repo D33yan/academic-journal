e599aa9818d438cb69259ba22185e4045894139e
D33yan <nnajidivine2006@gmail.com>
2024-07-10 04:34:24 +0100

    about page and journals page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,58 @@
+journals = [
+    {
+    title : 'Artificial Intelligence',
+    author : 'Prof. Pradipta Kumar Das',
+    description :' Artificial Intelligence refers to the simulation of human intelligence in machines that are programmed to think and learn like humans' ,
+    imagehref: '',
+    pdfhref:'',
 
+},
+    {
+    title : 'study of mobile app development industry',
+    author : 'abid bin syeed',
+    description :'App development that targets handheld devices has now become a standard by which services reach the target audience faster since almost everyone has access to a mobile device. The App Development industry has only grown in leaps and bounds ever since its inception. This paper discusses in detail the Mobile App Development Industry and its predominance in India. Objectives: To analyse and understand the Mobile App Development Industry worldwide , to understand the preference, usage, revenue, cost, and scope of App Development in India and to perform a SWOC Analysis for the Mobile App Development Industry.' ,
+    imagehref: '',
+    pdfhref:'',
 
- document.addEventListener('scroll',()=>{
-    const fadeIn = document.querySelectorAll('.fade-in');
-    const position = fadeIn.getBoundingClientRect();
-    if(position.top >= 0 && position.bottom <= window.innerHeight){
-        fadeIn.style.opacity = 1
-    }else{
-        fadeIn.style.opacity = 0
-    }
-})
-;
+},
+{
+    title:' Money management among university students ',
+    Author: 'Wan Rasyidah Wan Nawang,Syadiyah Abdul shukor',             
+    description: " Money matters are integral to our lives and for university students, these are an even more pressing aspect. Thus, it is important for university students to     have prudent and responsible money management as this can impact their  future and personal well-being. The study is set to meet its objective which is to investigate the impact of economic , social, and psychological factors on money management among university students. ",
+    imagehref: '',
+    pdfhref:'',
+},
+{
+    title:' Network Connections ',
+    Author: 'JohnN.Davies, Rich Picking, Vic Grout',             
+    description: " A visual representation of the interconnectedness of computer science, depicting a complex network of circuits, wires, and nodes. This image symbolizes the dynamic and ever-evolving nature of computer science, where innovative ideas and solutions are constantly emerging and connecting ",
+    imagehref: '',
+    pdfhref:'',
+},
+{
+    title:'The Landscape of User-centered Misinformation Interventions - A Systematic Literature Review ',
+    Author: 'Kevin Hart Wig, Rich Picking, Vic Grout',             
+    description: " A visual representation of the interconnectedness of computer science, depicting a complex network of circuits, wires, and nodes. This image symbolizes the dynamic and ever-evolving nature of computer science, where innovative ideas and solutions are constantly emerging and connecting ",
+    imagehref: '',
+    pdfhref:'',
+},
+{
+    title:'A Deep Learning Approach for Facial Emotion Recognition using Convolutional Neural Networks (CNNs',
+    Author: 'Dr. Sophia Patel',             
+    description: " This topic demonstrates a deep learning approach for facial emotion recognition using CNNs. The proposed model consists of convolutional and pooling layers to extract features from input images, followed by flatten and dense layers to classify emotions. The model is trained and tested on a dataset of facial images with different emotions. ",
+    imagehref: '',
+    pdfhref:'',
+},
+{
+    title:'website development and website designing',
+    Author: 'Priya surana',             
+    description: " Description: Websites today generate tremendous amounts of data and this data needs to be processed effectively by the creators. One such important factor is processing images on the website and generating effective data from it through techniques like Web Scraping. This journal explains the concept of web scraping and various other techniques used in web designing ",
+    imagehref: '',
+    pdfhref:'',
+},
+{
+    title: 'International Journal of Computer Technology and Electronics Engineering (IJCTEE)',
+    author: 'Ankita Gaur',
+    description: 'The International Journal of Computer Technology and Electronics Engineering(IJCTEE) is a peer-reviewed, open-access journal that publishes original research articles, reviews, and case studies in all areas of computer science and technology. The journal aims to provide a platform for researchers, academicians, and professionals to share their knowledge and expertise in the field.'
+},
+]
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const journals = [
+    {
+        title: 'Artificial Intelligence',
+        author: 'Prof. Pradipta Kumar Das',
+        description: 'Artificial Intelligence refers to the simulation of human intelligence in machines that are programmed to think and learn like humans.',
+        imagehref: './assets/images/ai.jpg',
+        pdfhref: './assets/pdfs/ai.pdf'
+    },
+    {
+        title: 'Study of Mobile App Development Industry',
+        author: 'Abid Bin Syeed',
+        description: 'App development that targets handheld devices has now become a standard by which services reach the target audience faster since almost everyone has access to a mobile device. The App Development industry has only grown in leaps and bounds ever since its inception. This paper discusses in detail the Mobile App Development Industry and its predominance in India. Objectives: To analyse and understand the Mobile App Development Industry worldwide, to understand the preference, usage, revenue, cost, and scope of App Development in India and to perform a SWOC Analysis for the Mobile App Development Industry.',
+        imagehref: './assets/images/mobile-app.jpg',
+        pdfhref: './assets/pdfs/mobile-app.pdf'
+    },
+    {
+        title: 'Study of Mobile App Development Industry',
+        author: 'Abid Bin Syeed',
+        description: 'App development that targets handheld devices has now become a standard by which services reach the target audience faster since almost everyone has access to a mobile device. The App Development industry has only grown in leaps and bounds ever since its inception. This paper discusses in detail the Mobile App Development Industry and its predominance in India. Objectives: To analyse and understand the Mobile App Development Industry worldwide, to understand the preference, usage, revenue, cost, and scope of App Development in India and to perform a SWOC Analysis for the Mobile App Development Industry.',
+        imagehref: './assets/images/mobile-app.jpg',
+        pdfhref: './assets/pdfs/mobile-app.pdf'
+    },
+]
